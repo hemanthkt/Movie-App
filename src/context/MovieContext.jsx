b7facee9d5672537a563fd422d21357460fcd3ec
hemanthkt <hmkt1503@gmail.com>
2025-01-02 16:24:00 +0530

    when favorit button cick the movies adds to the page

---
+++ src/context/MovieContext.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createContext, useEffect, useState, useContext } from "react";
+
+cosnt;
+
+function MovieContext() {
+  return <div>MovieContext</div>;
+}
+
+export default MovieContext;
